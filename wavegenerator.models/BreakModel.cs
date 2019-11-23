3fe390e1cbcbf98b54c4938551fc1f7edcc8e2d5
Hedonist <originalhedonist@gmail.com>
2019-11-23 19:49:50 +0000

    remove some unnecessary validations

--- wavegenerator.models/BreakModel.cs
+++ wavegenerator.models/BreakModel.cs
@@ -5,16 +5,6 @@ namespace wavegenerator
 {
     public class BreakModel
     {
-        public static BreakModel Default() => new BreakModel
-        {
-            MinTimeSinceStartOfTrack = TimeSpan.FromMinutes(10),
-            MinLength = TimeSpan.FromSeconds(2),
-            MaxLength = TimeSpan.FromSeconds(25),
-            MinTimeBetweenBreaks = TimeSpan.FromMinutes(5),
-            MaxTimeBetweenBreaks = TimeSpan.FromMinutes(30),
-            RampLength = TimeSpan.FromSeconds(5)
-        };
-
         [Range(typeof(TimeSpan), "00:00:30", "13:31:35")]
         public TimeSpan MinTimeSinceStartOfTrack { get; set; }
 
@@ -39,11 +29,9 @@ namespace wavegenerator
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var breakModel = (BreakModel)value;
-            var settings = (Settings)validationContext.ObjectInstance;
 
             if (breakModel.MaxTimeBetweenBreaks < breakModel.MinTimeBetweenBreaks) return new ValidationResult($"{nameof(BreakModel.MaxTimeBetweenBreaks)} must be >= {nameof(BreakModel.MinTimeBetweenBreaks)}");
             if (breakModel.MaxLength < breakModel.MinLength) return new ValidationResult($"{nameof(BreakModel.MaxLength)} must be >= {nameof(BreakModel.MinLength)}");
-            //if (breakModel.MaxLength + 2 * breakModel.RampLength > settings.Sections.TotalLength) return new ValidationResult($"{nameof(BreakModel.MaxLength)} + 2x{nameof(BreakModel.RampLength)} must be <= {nameof(Settings.Sections)}.{nameof(Settings.Sections.TotalLength)}");
             return ValidationResult.Success;
         }
         public override bool RequiresValidationContext => true;
--- wavegenerator.models/SectionModel.cs
+++ wavegenerator.models/SectionModel.cs
@@ -19,10 +19,10 @@ namespace wavegenerator
         [ValidateObject]
         public VarianceModel FeatureLengthVariance { get; set; }
 
-        [Range(typeof(TimeSpan), "00:00:01", "13:31:35")]
+        [Range(typeof(TimeSpan), "00:00:00", "13:31:35")]
         public TimeSpan MinRampLength { get; set; }
 
-        [Range(typeof(TimeSpan), "00:00:01", "13:31:35")]
+        [Range(typeof(TimeSpan), "00:00:00", "13:31:35")]
         public TimeSpan MaxRampLength { get; set; }
 
         [Required]
@@ -47,7 +47,6 @@ namespace wavegenerator
             if (sectionModel.MinFeatureLength > sectionModel.MaxFeatureLength) return new ValidationResult($"{prefix}: {nameof(SectionModel.MinFeatureLength)} must be <= {nameof(SectionModel.MaxFeatureLength)}");
             if (sectionModel.MinRampLength > sectionModel.MaxRampLength) return new ValidationResult($"{prefix}: {nameof(SectionModel.MinRampLength)} must be <= {nameof(SectionModel.MaxRampLength)}");
             if (sectionModel.MinRampLength * 2 + sectionModel.MaxFeatureLength > sectionModel.TotalLength) return new ValidationResult($"{prefix}: {nameof(SectionModel.MaxRampLength)}x2 + {nameof(SectionModel.MaxFeatureLength)} must be <= {nameof(SectionModel.TotalLength)}");
-            if (sectionModel.MinRampLength.TotalSeconds < 1) return new ValidationResult($"{prefix}: {nameof(SectionModel.MinRampLength)} must be at least 1 second");
             return ValidationResult.Success;
         }
 
