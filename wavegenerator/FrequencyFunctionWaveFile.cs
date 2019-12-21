bd8987f4e88a1e92232222fe80130b9198b88a3c
Hedonist <originalhedonist@gmail.com>
2019-12-21 11:59:41 +0000

    rounded tabletops poc

--- wavegenerator/FrequencyFunctionWaveFile.cs
+++ wavegenerator/FrequencyFunctionWaveFile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace wavegenerator
 {
@@ -9,11 +10,9 @@ namespace wavegenerator
         protected readonly double?[] lastAmplitudeGradient;
         protected readonly double?[] lastPeak; // the t of the last peak (either last top, or last bottom)
         protected readonly double?[] lastPeakAmplitude; //whether the last 'peak' was top or bottom
-        protected readonly double[] dt;
-        protected readonly int[] dn;
-        protected readonly double[] lastt;
-        protected readonly int?[] lastn;
         protected readonly bool phaseShiftChannels = false;
+        protected readonly bool[] inPeak;
+        protected readonly bool[] inTrough;
         public FrequencyFunctionWaveFile(bool phaseShiftChannels)
         {
             x = new double[Channels];
@@ -21,71 +20,74 @@ namespace wavegenerator
             lastAmplitudeGradient = new double?[Channels];
             lastPeak = new double?[Channels];
             lastPeakAmplitude = new double?[Channels];
-            dt = new double[Channels];
-            lastt = new double[Channels];
-            dn = new int[Channels];
-            lastn = new int?[Channels];
+            inPeak = new bool[Channels];
+            inTrough = new bool[Channels];
+
             this.phaseShiftChannels = phaseShiftChannels;
+
         }
 
         protected abstract double Frequency(double t, int n, int channel);
 
         protected virtual double PeakLength(double t, int n, int channel) => 0.0;
         protected virtual double TroughLength(double t, int n, int channel) => 0.0;
-        private double AmplitudeInternal(double t, int n, int channel)
+
+
+
+        public override double Amplitude(double t, int n, int channel)
         {
             //if (n == lastn[channel] && lastAmplitude[channel].HasValue)
             //    return lastAmplitude[channel].Value;
 
             double amplitude;
             var f = Frequency(t, n, channel);
-            if (n == lastn[channel] && lastAmplitude[channel].HasValue)
-                f /= 5;
+
+            if (inPeak[channel] && inTrough[channel]) throw new InvalidOperationException($"Sanity check failed.");
+
+            //if (inPeak[channel]) f /= 5;
+            //if (inTrough[channel]) f /= 2;
+
 
             var dx = 2 * Math.PI * f / Settings.SamplingFrequency;
             x[channel] += dx;
             amplitude = (phaseShiftChannels && channel == 1) ? Math.Cos(x[channel]) : Math.Sin(x[channel]);
             double? amplitudeGradient = null;
-            if (lastAmplitude[channel].HasValue) amplitudeGradient = amplitude - lastAmplitude[channel].Value;
+            if (lastAmplitude[channel].HasValue) amplitudeGradient = (amplitude - lastAmplitude[channel].Value) * Settings.SamplingFrequency;
 
             bool justReachedPeak = lastAmplitudeGradient[channel].HasValue && amplitudeGradient <= 0.5 && lastAmplitudeGradient[channel].Value > 0.5;
             bool justReachedTrough = lastAmplitudeGradient[channel].HasValue && amplitudeGradient >= -0.5 && lastAmplitudeGradient[channel] < -0.5;
+            if (justReachedPeak && justReachedTrough) throw new InvalidOperationException($"Sanity check failed.");
+
             if (justReachedPeak)
             {
-                lastPeak[channel] = t + dt[channel];
-                lastPeakAmplitude[channel] = 1;
-            }
-            else if (justReachedTrough)
-            {
-                lastPeak[channel] = t + dt[channel];
-                lastPeakAmplitude[channel] = -1;
+                if (inPeak[channel]) throw new InvalidOperationException($"Just reached a peak when already in one");
+                inPeak[channel] = true;
             }
 
-            lastn[channel] = n;
+            if (justReachedTrough)
+            {
+                if (inTrough[channel]) throw new InvalidOperationException($"Just reached a trough when already in one");
+                inTrough[channel] = true;
+            }
+
+            if (inPeak[channel])
+            {
+                var justLeftPeak = lastAmplitudeGradient[channel].HasValue && amplitudeGradient <= -0.5 && lastAmplitudeGradient[channel] > -0.5;
+                if (justLeftPeak) inPeak[channel] = false;
+            }
+
+            if (inTrough[channel])
+            {
+                var justLeftTrough = lastAmplitudeGradient[channel].HasValue && amplitudeGradient >= 0.5 && lastAmplitudeGradient[channel] < 0.5;
+                if (justLeftTrough) inTrough[channel] = false;
+            }
+
             lastAmplitude[channel] = amplitude;
             lastAmplitudeGradient[channel] = amplitudeGradient;
+
+
             return amplitude;
         }
 
-        public override double Amplitude(double t, int n, int channel)
-        {
-            //Due to the way wetness inverts, 'peaks' come out as 'troughs' and vice versa. Like looking at yourself in a mirror.
-            var invertedTroughLength = PeakLength(t, n, channel);
-            var invertedPeakLength = TroughLength(t, n, channel);
-            if (lastPeakAmplitude[channel] > 0 && lastPeak[channel] != null && t - lastPeak[channel] <= invertedPeakLength)
-            {
-                dt[channel] += t - lastt[channel]; // the delay gets longer while we're at the top
-                dn[channel]++;
-            }
-
-            if (lastPeakAmplitude[channel] < 0 && lastPeak[channel] != null && t - lastPeak[channel] <= invertedTroughLength)
-            {
-                dt[channel] += t - lastt[channel];
-                dn[channel]++;
-            }
-            double amplitude = AmplitudeInternal(t - dt[channel], n - dn[channel], channel);
-            lastt[channel] = t;
-            return amplitude;
-        }
     }
 }
--- wavegenerator/TabletopGenerator.cs
+++ wavegenerator/TabletopGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.IO;
 
 namespace wavegenerator
 {
@@ -12,11 +13,18 @@ namespace wavegenerator
 
     public abstract class TabletopGenerator : FrequencyFunctionWaveFile
     {
+        private readonly BufferedStream debugStream;
+        private readonly StreamWriter debugWriter;
+
         public TabletopGenerator(double baseFrequency, double sectionLengthSeconds, int numSections) : base(phaseShiftChannels: Settings.Instance.PhaseShiftPulses)
         {
             this.baseFrequency = baseFrequency;
             this.sectionLengthSeconds = sectionLengthSeconds;
             this.numSections = numSections;
+
+            debugStream = new BufferedStream(new FileStream("frequencydebug.csv", FileMode.Create, FileAccess.Write));
+            debugWriter = new StreamWriter(debugStream);
+
         }
 
         protected readonly double baseFrequency;
@@ -59,5 +67,70 @@ namespace wavegenerator
             if (p.RampLength < 0) throw new InvalidOperationException("RampLength must be >= 0");
             if (p.TopLength + 2 * p.RampLength > sectionLengthSeconds) throw new InvalidOperationException("TopLength + 2*RampLength must be <= sectionLengthSeconds");
         }
+
+
+        // put this back into frequency function wave file when finished debugging
+        public override double Amplitude(double t, int n, int channel)
+        {
+            //if (n == lastn[channel] && lastAmplitude[channel].HasValue)
+            //    return lastAmplitude[channel].Value;
+
+            double amplitude;
+            var f = Frequency(t, n, channel);
+
+            if (inPeak[channel] && inTrough[channel]) throw new InvalidOperationException($"Sanity check failed.");
+
+            //if (inPeak[channel]) f /= 5;
+            if (inTrough[channel]) f /= 5;
+
+
+            var dx = 2 * Math.PI * f / Settings.SamplingFrequency;
+            x[channel] += dx;
+            amplitude = (phaseShiftChannels && channel == 1) ? Math.Cos(x[channel]) : Math.Sin(x[channel]);
+            double? amplitudeGradient = null;
+            if (lastAmplitude[channel].HasValue) amplitudeGradient = (amplitude - lastAmplitude[channel].Value) * Settings.SamplingFrequency;
+
+            bool justReachedPeak = lastAmplitude[channel].HasValue && amplitude >= 0.8 && lastAmplitude[channel].Value < 0.8;
+            bool justReachedTrough = lastAmplitude[channel].HasValue && amplitude <= -0.8 && lastAmplitude[channel] > -0.8;
+            if (justReachedPeak && justReachedTrough) throw new InvalidOperationException($"Sanity check failed.");
+
+            int peakTroughNess = 0;
+
+            if (justReachedPeak)
+            {
+                if (inPeak[channel]) throw new InvalidOperationException($"Just reached a peak when already in one");
+                inPeak[channel] = true;
+            }
+
+            if (justReachedTrough)
+            {
+                if (inTrough[channel]) throw new InvalidOperationException($"Just reached a trough when already in one");
+                inTrough[channel] = true;
+            }
+
+            if (inPeak[channel])
+            {
+                peakTroughNess = 1;
+                var justLeftPeak = lastAmplitude[channel].HasValue && amplitude <= 0.8 && lastAmplitude[channel] > 0.8;
+                if (justLeftPeak) inPeak[channel] = false;
+            }
+
+            if (inTrough[channel])
+            {
+                peakTroughNess = -1;
+                var justLeftTrough = lastAmplitude[channel].HasValue && amplitude >= -0.8 && lastAmplitude[channel] < -0.8;
+                if (justLeftTrough) inTrough[channel] = false;
+            }
+
+
+
+            lastAmplitude[channel] = amplitude;
+            lastAmplitudeGradient[channel] = amplitudeGradient;
+
+            if (n % 100 == 0) debugWriter.WriteLine($"{t},{f},{amplitude},{peakTroughNess}");
+
+            return amplitude;
+        }
+
     }
 }
