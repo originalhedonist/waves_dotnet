cb16bb13756c6a9aabfbd624060f434debaf8cff
Hedonist <originalhedonist@gmail.com>
2020-01-11 18:06:44 +0000

    make featureprobability validation more descriptive add examplesettings.json

--- wavegenerator.models/FeatureProbabilityValidationAttribute.cs
+++ wavegenerator.models/FeatureProbabilityValidationAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace wavegenerator
 {
@@ -8,6 +9,10 @@ namespace wavegenerator
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var featureProbability = (FeatureProbability)value;
+            if(featureProbability == null)
+            {
+                throw new InvalidOperationException("Feature probability section must exist.");
+            }
             if(featureProbability.Frequency + featureProbability.PeaksAndTroughs + featureProbability.Wetness > 1)
             {
                 return new ValidationResult("Total FeatureProbability cannot be > 1.");
