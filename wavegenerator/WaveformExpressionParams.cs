c9c87ce04161ae82545fea2dc40b8e455e93c0a4
Hedonist <originalhedonist@gmail.com>
2020-02-03 17:39:37 +0000

    add roslyn, update test settings

---
+++ wavegenerator/WaveformExpressionParams.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wavegenerator
+{
+    public class WaveformExpressionParams
+    {
+        public double x { get; set; }
+    }
+}
