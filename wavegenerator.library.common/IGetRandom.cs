97e03508584ec2c8b09010dc3d4e73639d03ffd5
Hedonist <originalhedonist@gmail.com>
2020-03-16 09:51:16 +0000

    move breakapplier into common

--- wavegenerator.library.common/IGetRandom.cs
+++ wavegenerator.library.common/IGetRandom.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace wavegenerator.library.common
+﻿namespace wavegenerator.library.common
 {
     public interface IGetRandom
     {
